c21947d3e6b0a61b10ac0680649e43050c63c566
Dieryel Wade <utbowler839@gmail.com>
2024-09-03 13:24:45 -0400

    B: Created circleshape.py

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	screen.fill(COLOR, rect=None, special_flags=0)
-	
+
 	print("Starting asteroids!")
 	print("Screen width: 1280")
 	print("Screen height: 720")
 	return screen
 
 screen = main()
+
+clock = pygame.time.Clock()
+dt = 0
 running = True
 while running:
 	for event in pygame.event.get():
@@ -18,5 +21,6 @@ while running:
 			running = False
 	screen.fill(COLOR)
 	pygame.display.flip()
+	dt = clock.tick(60) / 1000.0
 
 		
